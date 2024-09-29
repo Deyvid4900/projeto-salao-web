0ed8bc110ea4c2a0acca04294627b809d5cbeb30
Deyvid Oliveira Silva <94657946+Deyvid4900@users.noreply.github.com>
2024-09-29 19:33:10 -0300

    voltei o horario page default

--- src/pages/Horarios/index.tsx
+++ src/pages/Horarios/index.tsx
@@ -1,23 +1,25 @@
-// // import React from "react"
-// // import Header from "../../components/Header/Header"
-// // import Sidebar from "../../components/Sidebar/Sidebar"
-// // import BG from "../../components/background/background"
+import React from "react"
+import Header from "../../components/Header/Header"
+import Sidebar from "../../components/Sidebar/Sidebar"
+import BG from "../../components/background/background"
 
-// // export const  Horarios=()=>{
+export const  Horarios=()=>{
+
+
   
-// //   return (
-// //     <>
-// //     <BG></BG>
-// //     <Header></Header>
-// //     <Sidebar></Sidebar>
-// //     <div className="d-flex flex-column justify-content-center align-items-center" style={{}}>
-// //         <div className="mt-5" style={{width:"90%"}}>
+  return (
+    <>
+    <BG></BG>
+    <Header></Header>
+    <Sidebar></Sidebar>
+    <div className="d-flex flex-column justify-content-center align-items-center" style={{}}>
+        <div className="mt-5" style={{width:"90%"}}>
         
-// //         </div>
-// //       </div> 
-// //     </>
-// //   )
-// // }
+        </div>
+      </div> 
+    </>
+  )
+}
 
 // // export default Horarios
 // import { useEffect } from 'react';
